58d145fa80cfce9678e7e493557f7c27d89698d6
Thomas van Lingen <mafndev@gmail.com>
2017-03-11 23:16:45 +0100

    feat: add k_puts function
    This is to be able to easily swap implementation of printing.

---
+++ src/kernel/include/kernel/print.h
@@ -0,0 +1,10 @@
+//
+// Created by mafn on 3/11/17.
+//
+
+#ifndef MAFN_KERNEL_PRINT_H
+#define MAFN_KERNEL_PRINT_H
+
+extern void k_puts(const char* str);
+
+#endif //MAFN_KERNEL_PRINT_H
--- src/kernel/mafn_kernel.c
+++ src/kernel/mafn_kernel.c
@@ -4,6 +4,7 @@
 
 #include <stdbool.h>
 
+#include <kernel/print.h>
 #include <kernel/vgaterm.h>
 #include <kernel/GDT.h>
 
@@ -11,12 +12,12 @@ void kernel_main()
 {
     vgaterm_init();
 
-    chardev_puts("Booting mafn kernel\n", &vgaterm);
-    chardev_puts("Initting GDT...", &vgaterm);
+    k_puts("Booting mafn kernel\n");
+    k_puts("Initting GDT...");
     mafn_kernel_gdt_init();
-    chardev_puts(" [DONE]\n", &vgaterm);
+    k_puts(" [DONE]\n");
 
-    chardev_puts("Entering infinite loop\n", &vgaterm);
+    k_puts("Entering infinite loop\n");
 
     while (true) {
         asm("nop");
